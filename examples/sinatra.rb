7567f0ea6677b0d4ed433731009e9990a4605bef
David Kuo <s50407s@gmail.com>
2014-05-02 01:57:04 +0800

    add log to find more feature and bug

--- examples/sinatra.rb
+++ examples/sinatra.rb
@@ -1,10 +1,16 @@
 require 'rubygems'
 require 'bundler'
+require 'logger'
 
 Bundler.setup :default, :development, :example
 require 'sinatra'
 require 'omniauth-ncu-portal-openid'
 require 'openid/store/filesystem'
+
+set :sessions, true
+set :logging, true
+set :environment, :production
+$log = Logger.new('log/login.txt', 'weekly')
 
 use Rack::Session::Cookie
 
@@ -15,7 +21,7 @@ end
 get '/' do
   <<-HTML
   <ul>
-    <li><a href='/auth/ncu_portal_open_id'>Sign in with OpenID</a></li>
+    <li><a href='/auth/ncu_portal_open_id'>Sign in with NCU Portal</a></li>
   </ul>
   HTML
 end
@@ -23,6 +29,7 @@ end
 [:get, :post].each do |method|
   send method, '/auth/:provider/callback' do
     content_type 'text/plain'
-    request.env['omniauth.auth'].info.to_hash.to_s
+    $log.info request.env['omniauth.auth'].info.to_hash.to_s
+    "Hi, #{request.env['omniauth.auth'].info[:student_id].to_s}. Thanks for test. <(_ _)>"
   end
 end
